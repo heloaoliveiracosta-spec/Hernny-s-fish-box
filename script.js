2e3fccf1cbeaef863a4933fb4a2763e4fbdbba6f
heloaoliveiracosta-spec <heloaoliveiracosta@gmail.com>
2025-12-09 23:51:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function pescar() {
   const sorteio = peixes[Math.floor(Math.random() * peixes.length)];
   document.getElementById("resultado").innerText = sorteio;
 }
+.escondido {
+  display: none;
+}
+
+.sobre {
+  animation: surgir 0.4s ease;
+}
+
+@keyframes surgir {
+  from {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
